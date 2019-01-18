088e9a622fb8f2a097de5f3a96c08188e2b221ab
saefulramdani <46807867+saefulramdani@users.noreply.github.com>
2019-01-18 14:30:46 +0700

    oop saeful

---
+++ 12.constant.php
@@ -0,0 +1,40 @@
+<?php 
+
+// define('NAMA', 'Saeful Ramdani');
+// echo NAMA;
+
+// echo "<br>";
+
+// const UMUR = 16;
+// echo UMUR;
+
+// class Coba {
+// 	const Nama = 'Saeful';
+// }
+
+
+// echo Coba::NAMA;
+
+
+// echo __FILE__;
+
+
+// function coba() {
+// 	return __FUNCTION__;
+// }
+
+
+// echo coba();
+
+
+class Coba {
+	public $kelas = __CLASS__;
+}
+
+$obj = new Coba;
+echo $obj->kelas;
+
+
+
+
+ ?>
